0342ffb3d2cf53dfec056b6db6460e21435eba6c
Ibrahim Ahmed <mohammadibrahimahmed135@gmail.com>
2022-07-12 05:51:54 -0700

    index.html created

---
+++ Head tail dice/script.js
@@ -0,0 +1,64 @@
+// document.write("<h1>Dice Game</h1>")
+
+// var DiceNumbers = [1, 2, 3, 4, 5, 6]
+// var RandomNumbers = Math.floor(Math.random() * DiceNumbers.length);
+// var RemovedZero = DiceNumbers.splice(RandomNumbers, 1);
+// document.write(RemovedZero)
+
+
+//    document.write("<h1>Password Generator</h1>")
+// var textArray = ["a", "A", "b", "c", "C", "d", "D", "E", 'e', "F", 'f', "g",
+//     "G", 'H', 'h', 'i', 'I', 'J', 'j', "K", "k", "L", "l", "M",
+//     "m", "N", "n", "O", "o", "P", "p", "Q", "q", "R", "r", "S", "s",
+//     "T", "t", "U", 'u', "V", 'v', "W", 'w', "x", "X", 'y', "Y", "Z", "z", 0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+// var list = [];
+
+
+//     for (i = 0; i <= 8; i++) {
+//         var randomNumber = Math.floor(Math.random() * textArray.length);
+
+//         var word = textArray[randomNumber];
+
+//         list.push(word)
+
+//     }
+
+//     var PasswordGenerated = list.splice(",").join("")
+
+//     document.write(PasswordGenerated)
+   
+
+
+    
+    
+    // document.write("<h1>Head And Tail Game</h1>")
+    
+    // var UserName = prompt("Enter you Name :")
+    
+    // var HeadTale = prompt("what Do you want? (H/T)");
+    
+    // var HT = ["H","T"] 
+    
+    // var randomNumber = Math.floor(Math.random() * HT.length);
+    
+    // var word = HT[randomNumber];
+    //     if(HeadTale === word){
+    //         document.write(UserName +" is winner");
+            
+    //     }
+    //     else if(HeadTale==""){
+            
+    //         alert("Empty Input")
+    //     }
+    //     else if(HeadTale!=="H" || HeadTale=="T"){
+    //         document.write  ("Niether Head nor Tail");
+    //     }
+       
+    
+    //     else{
+    //         document.write(UserName + " is Lose");
+    //     }
+
+   
+        
+          
